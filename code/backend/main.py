68c124d2546583a14bd243eef2adad5f2d9fdf22
Alvaro <alvaroguzman348@gmail.com>
2026-08-20 15:26:12 -0400

    Cors Abiertos

--- code/backend/main.py
+++ code/backend/main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,CORSMiddleware
 from database import Base, engine
 from app.pacientes.router import paciente
 from app.evaluaciones.router import evaluacion
 from app.pacientes.model.paciente import Paciente
 from app.evaluaciones.model.evaluacion import Evaluacion
 
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # o restringe a localhost y tu dominio
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(paciente.router)
 app.include_router(evaluacion.router)
