9b0895d620ae4dfe7350ce88a613ccb823d2796e
Alvaro <alvaroguzman348@gmail.com>
2026-09-07 18:54:13 -0400

    Declarar Colores y Tipografias constantes

--- code/frontend_flutter/lib/consts/colors.dart
+++ code/frontend_flutter/lib/consts/colors.dart
@@ -7,3 +7,10 @@ const Color blanco = Color.fromRGBO(255, 255, 255, 1);
 const Color plomoblanco = Color.fromRGBO(239, 239, 239, 1);
 const Color rojo = Color.fromRGBO(230, 46, 4, 1);
 const Color dorado = Color.fromRGBO(255, 168, 0, 1);
+
+const Color background = Color(0xFFF7F1E9);
+const Color primario = Color(0xFF313A1D);
+const Color secundario = Color(0xFF212B13);
+const Color auxiliar = Color(0xFFDCEB56);
+const Color error = Color(0xFFFF0000);
+const Color aviso = Color(0xFFFFC300);
